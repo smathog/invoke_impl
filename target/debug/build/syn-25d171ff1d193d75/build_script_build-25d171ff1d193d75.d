9b2fb5f38f4ff02f85c27de154c86459a43fdba5
Scott Mathog <smathog@gmail.com>
2022-06-07 12:11:26 -0700

    Updated to ignore target folder

--- target/debug/build/syn-25d171ff1d193d75/build_script_build-25d171ff1d193d75.d
+++
@@ -1,5 +0,0 @@
-/home/scott/Desktop/EMSim/invoker-macro/target/debug/build/syn-25d171ff1d193d75/build_script_build-25d171ff1d193d75: /home/scott/.cargo/registry/src/github.com-1ecc6299db9ec823/syn-1.0.96/build.rs
-
-/home/scott/Desktop/EMSim/invoker-macro/target/debug/build/syn-25d171ff1d193d75/build_script_build-25d171ff1d193d75.d: /home/scott/.cargo/registry/src/github.com-1ecc6299db9ec823/syn-1.0.96/build.rs
-
-/home/scott/.cargo/registry/src/github.com-1ecc6299db9ec823/syn-1.0.96/build.rs:
--- target/debug/deps/invoker_macro-64566ef0e490f9e8.d
+++
@@ -1,5 +0,0 @@
-/home/scott/Desktop/EMSim/invoker-macro/target/debug/deps/invoker_macro-64566ef0e490f9e8: src/lib.rs
-
-/home/scott/Desktop/EMSim/invoker-macro/target/debug/deps/invoker_macro-64566ef0e490f9e8.d: src/lib.rs
-
-src/lib.rs:
